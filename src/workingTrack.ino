da526c8a678588692f9ca4b8c67170ca05f999e9
JesusPartal <duncan.rsr@gmail.com>
2018-12-15 09:34:29 +0000

    Working in Firebase

--- src/workingTrack.ino
+++ src/workingTrack.ino
@@ -1,46 +1,50 @@
 #define CUSTOM_SETTINGS
+#define INCLUDE_INTERNET_SHIELD
 #define INCLUDE_GPS_SHIELD
 #define INCLUDE_TERMINAL_SHIELD
-#define INCLUDE_INTERNET_SHIELD
 
 #include <OneSheeld.h>
 
-float lat;
-float lng;
-char charlat [20];
-char charlng [20];
+float latitude;
+float longitude;
 
-char vehicleID [4] = "001";
-char vehicleStatus [2] = "a";
+char charLatitude [12];
+char charLongitude [12];
 
-char readings [80];
+char readings [40];
 
-HttpRequest myRequest ("https://jauc-ae38e.firebaseio.com/carTracking.json");
+char vehicleID [2] = "1";
+
+HttpRequest request("https://jauc-ae38e.firebaseio.com/carTracking");
 
 void setup() {
+  OneSheeld.begin();
+  OneSheeld.delay(2000);
   
-    OneSheeld.begin();
 }
 
 void loop() {
-
-    lat = GPS.getLatitude();
-    lng = GPS.getLongitude();
-    dtostrf(lat, 9, 5, charlat);
-    dtostrf(lng, 9, 5, charlng);
-    strcat(readings,"\"");
-    strcat(readings, vehicleID);
-    strcat(readings, ",");
-    strcat(readings, vehicleStatus);
-    strcat(readings, ",");
-    strcat(readings, charlat);
-    strcat(readings, ",");
-    strcat(readings, charlng);
-    strcat(readings, "\"");
-    Terminal.println(readings);
-    myRequest.addRawData(readings);
-    Internet.performPost(myRequest);
-    memset(readings, 0, sizeof(readings));
-    OneSheeld.delay(5000);
-    
+  //get latitude and longitude from the GPS Shield
+  latitude = GPS.getLatitude();
+  longitude = GPS.getLongitude();
+  //convert latitude and longitude to char
+  dtostrf(latitude, 11, 7, charLatitude);
+  dtostrf(longitude, 11, 7, charLongitude);
+  //concatenate charLatitude and charLongitude to readings
+  strcat(readings,"\"");
+  strcat(readings,charLatitude);
+  strcat(readings, ",");
+  strcat(readings,charLongitude);
+  strcat(readings, ",");
+  strcat(readings,vehicleID);
+  strcat(readings,"\"");
+  Terminal.println(readings);
+  // add the readings to the http request
+  request.addRawData(readings);
+  // perform post
+  Internet.performPost(request);
+  //clear readings
+  memset(readings, 0, sizeof(readings));
+  //wait 5 seconds
+  OneSheeld.delay(5000);
 }
